82153e6d82a0d66e3d8a01f3d406d07158dda315
Hassan Amir <hassanamirorg@gmail.com>
2024-07-24 15:28:56 +0500

    Update Website-1-Introduction

---
+++ website-1-introduction/index.js
@@ -0,0 +1,21 @@
+// This is a comment
+/* This is a multi-line comment 
+   This is a multi-line comment */
+
+// To print out text using console.log()
+// this will create a output in console section of 
+// dev tools
+
+// console.log("Hello World!") // Double quotes
+// console.log('Hello World!') // Single quotes
+// console.log(`Hello World!`) // Backticks - String litrals 
+
+// To print out text using window.alert()
+// this will create a pop-up
+
+// window.alert(`This is a alert message`)
+
+// To print out text using the textContent of an HTML
+// element on the website
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `My name is Hassan. You can call me Aech.`
