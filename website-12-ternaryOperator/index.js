bef63c4125353cb80952f8578f49c2032fa8413b
Hassan Amir <hassanamirorg@gmail.com>
2024-07-25 19:13:56 +0500

    Update Files Website-12-TernaryOperator

---
+++ website-12-ternaryOperator/index.js
@@ -0,0 +1,19 @@
+// ternaryOperator =>  a shortcut to if{} and else{} statements
+//                     helps to assign a variable based on a condition
+//                     condition ? codeIfTrue : codeIfFalse;
+
+// let age = 21;
+// let message = age >=18 ? `You're an Adult` : `You're a Minor'`;
+//     console.log(message);
+
+// let time = 15;
+// let greeting = time < 12 ? `Good Evening!` : `Good Afternoon!` ;
+//     console.log(greeting);
+
+// let isStudent = true;
+// let message = isStudent ? `You're a Student` : `You're not a Student`;
+//     console.log(message);
+
+// let purchasedAmount = 250;
+// let discount = purchasedAmount >= 100 ? 10 : 0;
+//     console.log(`You have ${discount}% discount.\nYou total amount is: ${purchasedAmount - purchasedAmount * (discount / 100)}`)
