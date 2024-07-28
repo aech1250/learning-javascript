27b10141d110a86df02d45bf74b080043d6bfea9
Hassan Amir <hassanamirorg@gmail.com>
2024-07-28 17:21:06 +0500

    Update Files

---
+++ 18-js-strictEquality/index.js
@@ -0,0 +1,20 @@
+// =   assignment operator
+// ==  comparison operator (compare if values are equal)
+// === strict equality operator (compare if values & datatype are equal)
+// !=  inequality operator
+// !== strict inequality operator
+
+// const PI = 3.14;
+
+// if(PI !== "3.14"){
+//     console.log(`This is PI. Because JS thick datatypes are same.`)
+// }
+// if(PI === 3.14){
+//     console.log(`This is PI. Because JS thick both value and datatypes are same.`)
+// }
+// if (PI == 3.14 && PI == "3.14"){
+//     console.log(`This is PI. Because JS thick value are same and doesn't consider the datatype.`)
+// }
+// if(PI != 1.23){
+//     console.log(`This is not PI.`)
+// }
