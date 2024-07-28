0b636e3731f7f92dd5101dbf1d379271ca10dfcf
Hassan Amir <hassanamirorg@gmail.com>
2024-07-28 16:03:17 +0500

    Update Files

---
+++ 14-js-stringMethods/index.js
@@ -0,0 +1,17 @@
+// String Method => These allow you to manipulate text (string).
+
+let userName = "Hassan Amir";
+
+    console.log(userName.charAt(2));
+    console.log(userName.indexOf("s"));
+    console.log(userName.length);
+    console.log(userName.trim());
+    console.log(userName.toUpperCase());
+    console.log(userName.toLowerCase());
+    console.log(userName.repeat(2));
+    console.log(userName.startsWith("H"));
+    console.log(userName.endsWith("r"));
+    console.log(userName.includes(" "));
+    console.log(userName.replaceAll(" ", ""));
+    console.log(userName.padStart(12, "_"));
+    console.log(userName.padEnd(12, "_"));
