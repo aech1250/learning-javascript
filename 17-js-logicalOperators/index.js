817dd01adee5eabaf7109db0c3d183a20325e885
Hassan Amir <hassanamirorg@gmail.com>
2024-07-28 17:04:42 +0500

    Update Files

---
+++ 17-js-logicalOperators/index.js
@@ -0,0 +1,27 @@
+// logical operator => used to combine or manipulate
+//                     boolean value (true or false)
+
+//                     There are:
+//                     1. && (AND Operator)
+//                     2. || (OR Operator)
+//                     1. !  (NOT Operator)
+
+// let temp = 41;
+
+// if(temp < 0 || temp > 40){
+//     console.log(`Temperature is BAD!`);
+// }else if(temp > 30){
+//     console.log(`Temerature is Hot`);
+// }else if(temp > 20){
+//     console.log(`Temerature is Warm`);
+// }else{
+//     console.log(`Temerature is Cold`);
+// }
+
+// let isSunny = true;
+
+// if(!isSunny){
+//     console.log(`It is Cloudy.`)
+// }else{
+//     console.log(`It is Sunny.`)
+// }
