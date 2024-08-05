ec0edacea38734456c693277372aabe87320598c
Hassan Amir <hassanamirorg@gmail.com>
2024-08-05 17:31:56 +0500

    Update Files

--- 68-js-rockPaperScissors/index.js
+++ 68-js-rockPaperScissors/index.js
@@ -24,37 +24,33 @@ function playGame(choice){
     const random = Math.floor((Math.random() * 3) + 1);
     const computerChoice = choices[random - 1];
 
-    if (   (choice === 'rock' && computerChoice === 'rock') 
-        || (choice === 'paper' && computerChoice === 'paper') 
-        || (choice === 'scissor' && computerChoice === 'scissor') ) {
+    if (choice === computerChoice) {
         playerChoiceDisplay.textContent = `Player: ${choice}`;
         computerChoiceDisplay.textContent = `Computer: ${computerChoice}`;
         resultDisplay.textContent = `Its a tie`;
         resultDisplay.style.visibility = "visible";
         resultDisplay.style.color = "rgb(165, 165, 165)";
-    }
-    if (   (choice === 'rock' && computerChoice === 'scissor')
-        || (choice === 'scissor' && computerChoice === 'paper')
-        || (choice === 'paper' && computerChoice === 'rock')) {
-        playerChoiceDisplay.textContent = `Player: ${choice}`;
-        computerChoiceDisplay.textContent = `Computer: ${computerChoice}`;
-        resultDisplay.textContent = `You won`;
-        resultDisplay.style.visibility = "visible";
-        resultDisplay.style.color = "rgb(65, 255, 65)";
-        humanPlayer.increment();
-        playerScoreDisplay.textContent = humanPlayer.score;
-        computerScoreDisplay.textContent = computerPlayer.score;
-    }
-    if (   (choice === 'rock' && computerChoice === 'paper')
-        || (choice === 'scissor' && computerChoice === 'rock')
-        || (choice === 'paper' && computerChoice === 'scissor')) {
-        playerChoiceDisplay.textContent = `Player: ${choice}`;
-        computerChoiceDisplay.textContent = `Computer: ${computerChoice}`;
-        resultDisplay.textContent = `You lose`;
-        resultDisplay.style.visibility = "visible";
-        resultDisplay.style.color = "rgb(255, 46, 46)";
-        computerPlayer.increment();
-        playerScoreDisplay.textContent = humanPlayer.score;
-        computerScoreDisplay.textContent = computerPlayer.score;
+    }else{
+        if (   (choice === 'rock' && computerChoice === 'scissor')
+            || (choice === 'scissor' && computerChoice === 'paper')
+            || (choice === 'paper' && computerChoice === 'rock')) {
+            playerChoiceDisplay.textContent = `Player: ${choice}`;
+            computerChoiceDisplay.textContent = `Computer: ${computerChoice}`;
+            resultDisplay.textContent = `You won`;
+            resultDisplay.style.visibility = "visible";
+            resultDisplay.style.color = "rgb(65, 255, 65)";
+            humanPlayer.increment();
+            playerScoreDisplay.textContent = humanPlayer.score;
+            computerScoreDisplay.textContent = computerPlayer.score;
+        }else{
+            playerChoiceDisplay.textContent = `Player: ${choice}`;
+            computerChoiceDisplay.textContent = `Computer: ${computerChoice}`;
+            resultDisplay.textContent = `You lose`;
+            resultDisplay.style.visibility = "visible";
+            resultDisplay.style.color = "rgb(255, 46, 46)";
+            computerPlayer.increment();
+            playerScoreDisplay.textContent = humanPlayer.score;
+            computerScoreDisplay.textContent = computerPlayer.score;
+        }
     }
 }
