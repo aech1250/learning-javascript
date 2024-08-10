a64ffed565a904a69dfa1a520cc15bedb882b74b
Hassan Amir <96637885+aech1250@users.noreply.github.com>
2024-08-10 23:41:37 +0500

    Update index.js

--- 75-js-weatherAppProject/index.js
+++ 75-js-weatherAppProject/index.js
@@ -1,7 +1,7 @@
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
-const apiKey = "088414da65d8840efab6031672356711";
+const apiKey = "";
 
 weatherForm.addEventListener("submit" || "enter", async event => {
 
